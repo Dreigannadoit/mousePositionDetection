d685914da7193bc7de9d65ac9e2f1c324df516da
Drei Abmab <dreiabmab@gmail.com>
2024-02-07 22:08:01 +0800

    Feb 07 02 - 3

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const eyes = document.querySelectorAll('.move');
 const cursor = document.getElementById('cursor')
+const main = document.getElementById('main')
 
 let mousePosX, mousePosY;
+let makeCenterX, makeCenterY;
 
-window.addEventListener('mousemove', (e) => {
-    mousePosX = e.clientX;  // X position of mouse
-    mousePosY = e.clientY;  // Y position of mouse
+const mousePositionUpdate = (event) => {
+    mousePosX = event.clientX;
+    mousePosY = event.clientY;
 
-    const makeCenterX = mousePosX - window.innerWidth / 2;   // center X position
-    const makeCenterY = mousePosY - window.innerHeight / 2;  // center Y position
-    
+    makeCenterX = mousePosX - window.innerWidth / 2;
+    makeCenterY = mousePosY - window.innerHeight / 2;
+}
+
+const getEye = () =>{
     eyes.forEach((eye) => {
         let speedx = eye.dataset.speedx;
         let speedy = eye.dataset.speedy;
         eye.style.transform = `translateX(calc(-50% + ${makeCenterX * speedx}px)) translateY(calc(-50% + ${makeCenterY * speedy}px))`;
     });
+}
+
+const moveEyes = main.addEventListener('mousemove', (e) => {
+    mousePositionUpdate(e);
+    getEye()
+});
+
+const cantSee = main.addEventListener('mouseout', (e) => {
+    mousePositionUpdate(e);
+    eyes.forEach((eye) => {
+        eye.style.transform = `translateX(-50%) translateY(-50%)`;
+    });
+});
+
+
+const moveCursor = window.addEventListener('mousemove', (e) => {
+    mousePositionUpdate(e);
 
     cursor.style.transform = `translateX(calc(-50% + ${makeCenterX}px)) translateY(calc(-50% + ${makeCenterY}px))`;
-    
 });
